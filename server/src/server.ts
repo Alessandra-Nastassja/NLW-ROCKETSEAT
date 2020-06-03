a849888771455fb50339646a7a782e3b6d0c0f59
alessandra.nsantos <alessandra.nsantos@b2wdigital.com>
2020-06-02 21:24:47 -0300

    back: Aprendendo express

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,48 @@
-import express from 'express';
+import express, { response, request } from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+const users = [
+    'Diego',
+    'Cleiton',
+    'Robson',
+    'Daniel'
+];
+
+// Rotas 
 app.get('/users', (request, response) => {
     console.log('Listagem dos usuários');
 
-    response.send([
-        'Diego',
-        'Cleiton',
-        'Robson',
-        'Daniel'
-    ]);
+    const search = String(request.query.search);
+
+    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+    
+    return response.json(filteredUsers);
 })
 
-app.listen(3333);
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id);
+
+    const user = users[id];
+
+    return response.json(user);
+})
+
+app.post('/users', (request, response) => {
+    console.log('Criação do usuário');
+
+    const data = request.body;
+
+    console.log(data);
+    
+    const user = {
+        name: data.name,
+        email: data.email
+    };
+
+    return response.json(user);
+})
+
+app.listen(3334);
