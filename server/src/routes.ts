d2b4f62b5ed66b38e2ecc05f63edf94db0c9eb01
alessandra.nsantos <alessandra.nsantos@b2wdigital.com>
2020-06-07 12:53:04 -0300

    back: Validação da entrada de dados

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,12 @@
 import express, { request, response } from 'express';
 import multer from 'multer';
+import { celebrate, Join } from 'celebrate';
 
 import multerConfig from './config/multer';
 
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
+import Joi from '@hapi/joi';
 
 // Substitui o app 
 const routes = express.Router();
@@ -15,7 +17,23 @@ const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
 // POST - Cria pontos de coletas e fazendo upload de image
-routes.post('/points', upload.single('image'),pointsController.create);
+routes.post('/points', 
+    upload.single('image'),  
+    celebrate({
+        body: Joi.object().keys({
+            name: Joi.string().required(),
+            email: Joi.string().required().email(),
+            whatsapp: Joi.number().required(),
+            latitude: Joi.number().required(),
+            longitude: Joi.number().required(),
+            city: Joi.string().required(),
+            uf: Joi.string().required().max(2),
+            items: Joi.string().required(),
+        })
+    },{
+        abortEarly: false
+    }),
+    pointsController.create);
 
 // GET - Busca pelas informações dos items
 routes.get('/items', itemsController.index);
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import cors from 'cors';
+import { errors } from 'celebrate';
 
 import routes from './routes';
 
@@ -13,6 +14,8 @@ app.use(express.json());
 app.use(routes);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+
+app.use(errors());
 
 const port = process.env.port || 3334;
 app.listen(port);
