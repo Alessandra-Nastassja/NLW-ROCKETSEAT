688ef253ef13974a8010c7313df164a7783a82bc
alessandra.nsantos <alessandra.nsantos@b2wdigital.com>
2020-07-01 21:29:05 -0300

    Criação da busca por ponto

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiSearch } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 
 import logo from '../../assets/logo.svg';
@@ -8,23 +8,38 @@ import './styles.css';
 
 const Home = () => {
     return (
-        <div id="page-home">
-            <div className="content">
-                <header>
-                    <img src={logo} alt="Logo do Ecoleta"/>
-                </header>
-                <main>
-                     <h1>Seu marketplace de coleta de resíduos.</h1>
-                     <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
-                     <Link to="/create-point">
-                         <span>
-                             <FiLogIn/>
-                         </span>
-                         <strong>Pesquisar pontos de coleta</strong>
-                     </Link>
-                </main>
+        <>
+            <div id="page-home">
+                <div className="content">
+                    <header>
+                        <img src={logo} alt="Logo do Ecoleta"/>
+                        <Link to="/create-point">
+                            <FiLogIn/>
+                            Cadastre um ponto
+                        </Link>
+                    </header>
+                    <main>
+                        <h1>Seu marketplace de coleta de resíduos.</h1>
+                        <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
+                        <Link to="/list-point">
+                            <span>
+                                <FiSearch/>
+                            </span>
+                            <strong>Pesquisar pontos de coleta</strong>
+                        </Link>
+                    </main>
+                </div>
             </div>
-        </div>
+            <div id="popup-home">
+                <div className="content">
+                    <label>Pontos de coleta</label>
+                    <div>
+                        <input type="text" placeholder="Pesquise por cidade"/>
+                        <FiSearch/>
+                    </div>
+                </div>
+            </div>         
+        </>
     );
 }
 
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { Route, BrowserRouter } from 'react-router-dom';
 
 import Home from './pages/Home/index';
-import CreatePoint from './pages/CreatePoint'
+import CreatePoint from './pages/CreatePoint';
+import ListPoint from './pages/ListPoint';
 
 const Routes = () => {
     return (
         <BrowserRouter>
             <Route component={Home} path="/" exact/>
             <Route component={CreatePoint} path="/create-point" />
+            <Route component={ListPoint} path="/list-point" />
         </BrowserRouter>
     );
 }
